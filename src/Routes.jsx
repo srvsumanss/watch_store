8dee2e0846d37a9b4ddee9cafc6d760d0946074a
Saurabh Suman <96100046+srvsumanss@users.noreply.github.com>
2022-05-21 23:47:25 +0530

    update

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,8 +5,8 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Product from './pages/Products';
 import Filter from './pages/Filter';
-import Cart from './pages/Cart';
-import Card from './pages/Card';
+// import Cart from './pages/Cart';
+// import Card from './pages/Card';
 const Router = () => {
   return (
     <Routes>
@@ -15,8 +15,8 @@ const Router = () => {
         <Route path='/Signup' element={<Signup/>}></Route>
         <Route path='/Products' element={<Product/>}></Route>
         <Route path='/Filter' element={<Filter/>}></Route>
-        <Route path='/Cart' element={<Cart/>}></Route>
-        <Route path='/Card' element={<Card/>}></Route>
+        {/* <Route path='/Cart' element={<Cart/>}></Route> */}
+        {/* <Route path='/Card' element={<Card/>}></Route> */}
     </Routes>
 
   )
